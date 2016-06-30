cdf62a6789e36d2cf905ccccaa93eb468026de42
Thomas Hobohm <superman3275@gmail.com>
2016-06-29 22:27:04 -0500

    Supported more file formats.

--- app/models/speech.rb
+++ app/models/speech.rb
@@ -1,5 +1,5 @@
 class Speech < ActiveRecord::Base
   belongs_to :room
   has_attached_file :speech_doc
-  validates_attachment :speech_doc, :content_type => { :content_type => %w(application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document) }
+  validates_attachment :speech_doc, :content_type => { :content_type => %w(application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document application/pdf application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet text/plain) }
 end
